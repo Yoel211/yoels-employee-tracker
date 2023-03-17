558d274b94855f6bf024bbc4715879cca6b42cf7
Yoel Regalado <yoelregalado7@gmail.com>
2023-03-16 22:45:03 -0600

    added query to sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,7 @@ VALUES ("Web Development"),
        ("Electives");
 
 
-INSERT INTO roles (name, department)
+INSERT INTO roles (name, department_id)
 VALUES ("Intro to JavaScript", 1),
        ("Data Science", 2),
        ("Linear Algebra"),
@@ -14,11 +14,11 @@ VALUES ("Intro to JavaScript", 1),
        ("Game Design", 1),
        ("Cloud Development", 1);
 
-INSERT INTO employee (name,roles)
-VALUES ("Jon",1),
-       ("Brian",1),
-       ("Yoel",2),
-       ("Aachal",2),
-       ("Avery",3),
-       ("Enter name here",3),
-       ("Cloud",4);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES ("Jon", "M" , 1, 10),
+       ("Brian", "S" , 1 , 10),
+       ("Yoel", "R" , 2 , 10),
+       ("Aachal", "A" , 2),
+       ("Avery", "L" , 3),
+       ("Enter name here", "X" , 3),
+       ("Cloud", "L" ,4);
